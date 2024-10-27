cf74341d7a24cbfd3b415603444cc0083b3ee7e0
OmarFaig <omerfaiq7@gmail.com>
2024-10-27 15:40:47 +0100

    first cd files

--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace WeatherAPP
+{
+
+    public class Programm
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the WeatherAPP");
+        }
+    }
+}
